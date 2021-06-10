73436bd74552a8639f713a746cfa845c81e87cec
Zahid Rabbi <alzahidrabbi@gmail.com>
2021-06-11 00:30:51 +0600

    nav completed

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
+import Nav from '../components/Nav'
 
 export default function Home() {
   return (
@@ -11,10 +12,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {/* Header */}
       <Header/>
+      <Nav />
 
-      {/* Nav */}
 
       {/* Results */}
     </div>
